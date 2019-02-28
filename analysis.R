2ba3665d9c4295b399f14dd85a9f68e3383b5a80
Etienne Peillard <etienne.peillard@ec-nantes.fr>
2019-02-28 16:22:51 +0100

    full factorial model

--- analysis.R
+++ analysis.R
@@ -11,42 +11,38 @@ afex_options(emmeans_model = "multivariate") # use multivariate model for all fo
 
 d <- read.table('trimmedData.csv',header = TRUE, sep = ',')
 
-d$realIsRight <- factor(d$realIsRight)
-d$rep <- factor(d$rep)
 d$ans <- d$ans*100
 
+#trim data to delete errors
 d$relativeAns <- d$ans/d$e
 d<-d[d$relativeAns>0.35,]
 
+#full factorial model
+d$realIsRight <- factor(d$realIsRight)
+d$rep <- factor(d$rep)
+d$e <- factor(d$e)
+
+# mean between the two rep
 d<-aggregate(ans~realIsRight+e+type+id,data=d,FUN=mean)
 
 #ggplot(data=d, aes(x=e, y=relativeAns, colour=factor(type)))+ stat_smooth(method=lm, fullrange=FALSE) + geom_point()
 
-
+# test if the VR/RV factor is relevent
 dVR <- d[which(d$type=="RV"),]
 boxplot(dVR$ans ~ dVR$realIsRight)
 fitRV <- lm(ans ~ e + realIsRight, data=d)
 summary(fitRV)
 anova(fitRV)
 
-
+# since the anova is not significant, we delete this model and average the results
 d<-aggregate(ans~e+type+id,data=d,FUN=mean)
 
 
+#some plots
+d$relativeAns <- d$ans/d$e
+
 boxplot(d$ans ~ d$id)
 boxplot(d$relativeAns ~ d$id)
-
-#shapiro.test(d$relativeAns)
-
-#fit1 <- lm(ans ~ e + type*realIsRight, data=d)
- #summary(fit1)
-#ano1<-aov(fit1)
-#TukeyHSD(ano1)
-
-fit2 <- lm(ans ~ e + type, data=d)
-summary(fit2)
-ano2<-aov(fit2)
-summary(ano2)
 
 boxplot(d$ans ~ d$e)
 boxplot(d$relativeAns ~ d$e)
@@ -54,17 +50,34 @@ boxplot(d$relativeAns ~ d$e)
 boxplot(d$ans ~ d$type)
 boxplot(d$relativeAns ~ d$type)
 
-layout(matrix(1:4,2,2))
-plot(fit1)
+### Old model, not relevent
+#fit1 <- lm(ans ~ e + type*realIsRight, data=d)
+#summary(fit1)
+#ano1<-aov(fit1)
+#TukeyHSD(ano1)
 
+# fit2 <- lm(ans ~ e + type, data=d)
+# summary(fit2)
+# ano2<-aov(fit2)
+# summary(ano2)
+
+# layout(matrix(1:4,2,2))
+# plot(fit1)
+
+### plot the linear fit
 par(mfrow=c(1,1))
 plot <- ggplot(data=d, aes(x=e, y=ans, colour=factor(type)))
 plot + stat_smooth(method=lm, fullrange=FALSE) + geom_point()
 plot + stat_smooth(method=lm, fullrange=FALSE) + geom_point() + facet_wrap(~as.factor(id), nrow=5)
 
-
+### compute anova
 a1 <- aov_ez("id", "ans", d, between = NULL, within = c("e","type"),anova_table = list(es = "pes"))
 nice(a1$anova_table)
 
-m1 <- emmeans(a1$aov, ~ type+e)
+### do post-hoc
+m1 <- emmeans(a1$aov, ~ type)
 summary(as.glht(pairs(m1)), test=adjusted("bonferroni"))
+m2 <- emmeans(a1$aov, ~ e)
+summary(as.glht(pairs(m2)), test=adjusted("bonferroni"))
+m3 <- emmeans(a1$aov, ~ type+e)
+summary(as.glht(pairs(m3)), test=adjusted("bonferroni"))
